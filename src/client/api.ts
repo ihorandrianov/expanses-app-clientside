a5577c638e86fee2cde7bc68aba997adbf80aa43
Ihor Andrianov <andrianoia@gmail.com>
2022-11-14 01:49:49 +0200

    fixes for deploy

--- src/client/api.ts
+++ src/client/api.ts
@@ -30,7 +30,7 @@ export interface VerificationToken {
   token: string;
 }
 
-const DEFAULT_URL = 'expanses-app-production.up.railway.app';
+const DEFAULT_URL = 'https://expanses-app-production.up.railway.app';
 
 const client = axios.create({
   withCredentials: true,
