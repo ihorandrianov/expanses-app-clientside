b23427c984191d4e9ec5bbfa85c9a148293ded2b
Ihor Andrianov <andrianoia@gmail.com>
2022-11-14 17:18:25 +0200

    ___dirname fix

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { join } from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(join(__dirname, 'dist')));
 
